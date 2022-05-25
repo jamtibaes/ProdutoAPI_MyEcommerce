9b3368272ea581d799d7dd8222ba63cad7d7a3c6
jamtibaes <jamtibaes@yahoo.com.br>
2022-05-25 20:10:19 -0300

    Initial commit

---
+++ src/main/java/com/letscode/ecommerce/produtoapi/EcommerceProdutoApiApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProdutoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProdutoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letscode/ecommerce/produtoapi/EcommerceProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.produtoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProdutoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
